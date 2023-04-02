a23d8e1d4228f2f43ca2fcc220dad6fca8b09bf4
Jochen Bartl <jochenbartl@mailbox.org>
2023-04-02 21:11:07 +0200

    converted css_color_convert to uint32

--- bits_and_bytes/css_color_convert/bin/main.ml
+++ bits_and_bytes/css_color_convert/bin/main.ml
@@ -1,17 +1,6 @@
-exception Mailformed_data of string
+open Unsigned
 
-type rgb_type =
-  | Rgb of {
-      r: int;
-      g: int;
-      b: int;
-    }
-  | Rgb_alpha of {
-      r: int;
-      g: int;
-      b: int;
-      a: int;
-    }
+exception Mailformed_data of string
 
 let int_of_hex_char c =
   match c with
@@ -20,40 +9,58 @@ let int_of_hex_char c =
   | 'A' .. 'F' -> Char.code c - 55
   | _ -> invalid_arg (Printf.sprintf "char %c is not a valid hex digit" c)
 
-let make_int s i j =
-  let x = int_of_hex_char (String.get s i) in
-  let y = int_of_hex_char (String.get s j) in
-  (x lsl 4) lxor y
+let uint32_of_hex_string s si sj =
+  let x = int_of_hex_char (String.get s si) in
+  let y = int_of_hex_char (String.get s sj) in
+  UInt32.of_int ((x lsl 4) lxor y)
 
+(* encode hexadecimal string into UInt32 of rrggbbaa
+ * - aa of 00 is a regular RGB value
+ *)
 let hexstr_to_rgb s =
+  let i = Unsigned.UInt32.zero in
   match String.length s with
-  | 3 -> Rgb { r = make_int s 0 0; g = make_int s 1 1; b = make_int s 2 2 }
-  | 6 -> Rgb { r = make_int s 0 1; g = make_int s 2 3; b = make_int s 4 5 }
+  | 3 ->
+    let i = UInt32.(logxor i (shift_left (uint32_of_hex_string s 0 0) 24)) in
+    let i = UInt32.(logxor i (shift_left (uint32_of_hex_string s 1 1) 16)) in
+    let i = UInt32.(logxor i (shift_left (uint32_of_hex_string s 2 2) 8)) in
+    i
+  | 6 ->
+    let i = UInt32.(logxor i (shift_left (uint32_of_hex_string s 0 1) 24)) in
+    let i = UInt32.(logxor i (shift_left (uint32_of_hex_string s 2 3) 16)) in
+    let i = UInt32.(logxor i (shift_left (uint32_of_hex_string s 4 5) 8)) in
+    i
   | 4 ->
-    Rgb_alpha
-      {
-        r = make_int s 0 0;
-        g = make_int s 1 1;
-        b = make_int s 2 2;
-        a = make_int s 3 3;
-      }
+    let i = UInt32.(logxor i (shift_left (uint32_of_hex_string s 0 0) 24)) in
+    let i = UInt32.(logxor i (shift_left (uint32_of_hex_string s 1 1) 16)) in
+    let i = UInt32.(logxor i (shift_left (uint32_of_hex_string s 2 2) 8)) in
+    let i = UInt32.(logxor i (uint32_of_hex_string s 3 3)) in
+    i
   | 8 ->
-    Rgb_alpha
-      {
-        r = make_int s 0 1;
-        g = make_int s 2 3;
-        b = make_int s 4 5;
-        a = make_int s 6 7;
-      }
+    let i = UInt32.(logxor i (shift_left (uint32_of_hex_string s 0 1) 24)) in
+    let i = UInt32.(logxor i (shift_left (uint32_of_hex_string s 2 3) 16)) in
+    let i = UInt32.(logxor i (shift_left (uint32_of_hex_string s 4 5) 8)) in
+    let i = UInt32.(logxor i (uint32_of_hex_string s 6 7)) in
+    i
   | _ -> raise (Mailformed_data s)
 
 let alpha_float_of_int i = Printf.sprintf "%.5f" (float_of_int i /. 255.)
 
-let rgb_to_string = function
-  | Rgb rgb -> Printf.sprintf "color: rgb(%i %i %i)" rgb.r rgb.g rgb.b
-  | Rgb_alpha rgba ->
-    Printf.sprintf "color: rgba(%i %i %i / %s)" rgba.r rgba.g rgba.b
-      (alpha_float_of_int rgba.a)
+let has_alpha i = UInt32.(not (equal (logand i (of_int 0b1111_1111)) zero))
+
+let get_color_value i n =
+  let x = UInt32.(logand i (shift_left (of_int 0b1111_1111) n)) in
+  let x = UInt32.shift_right x n in
+  UInt32.to_int x
+
+let rgb_to_string i =
+  if has_alpha i then
+    Printf.sprintf "color: rgba(%i %i %i / %s)" (get_color_value i 24)
+      (get_color_value i 16) (get_color_value i 8)
+      (alpha_float_of_int (get_color_value i 0))
+  else
+    Printf.sprintf "color: rgb(%i %i %i)" (get_color_value i 24)
+      (get_color_value i 16) (get_color_value i 8)
 
 let css_color_regex = Re.Pcre.regexp {|color: #([a-zA-Z0-9]{3,8})|}
 
@@ -75,8 +82,8 @@ let read_from_stdin () =
   Out_channel.(flush stdout)
 
 let () =
-  (* assert (hexstr_to_rgb "123" = Rgb { r = 17; g = 34; b = 51 }); *)
-  (* assert (hexstr_to_rgb "00ff00" = Rgb { r = 0; g = 255; b = 0 }); *)
-  (* assert (hexstr_to_rgb "00f8" = Rgb_alpha { r = 0; g = 0; b = 255; a = 136 }); *)
-  (* assert (hexstr_to_rgb "0000FFC0" = Rgb_alpha { r = 0; g = 0; b = 255; a = 192 }); *)
+  (* assert (hexstr_to_rgb "123" = UInt32.of_string "287453952"); *)
+  (* assert (hexstr_to_rgb "00ff00" = UInt32.of_string "16711680"); *)
+  (* assert (hexstr_to_rgb "00f8" = UInt32.of_string "65416"); *)
+  (* assert (hexstr_to_rgb "0000FFC0" = UInt32.of_string "65472") *)
   read_from_stdin ()
